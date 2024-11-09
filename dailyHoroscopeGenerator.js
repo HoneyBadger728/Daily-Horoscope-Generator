94f071c6d5062eb040f0b04bb52be562324af096
bDavis <BlakelyJDavis@gmail.com>
2024-11-09 18:39:22 -0500

    added financial outlook options

--- dailyHoroscopeGenerator.js
+++ dailyHoroscopeGenerator.js
@@ -1,7 +1,14 @@
 const dayOutlook = () => {
-    const outlooks = [`like a punch to the face`, `a breath of fresh air`, `full of learning opportunities`]
-    return outlooks[Math.floor(Math.random() * 3)]
+    const dayOutlooks = [`like a punch to the face`, `a breath of fresh air`, `full of learning opportunities`];
+    return dayOutlooks[Math.floor(Math.random() * 3)]
 }
+
+const financialOutlook = () => {
+    const financialOutlooks = [' expect to experience a not-so-great depression', `soon be enjoying champagne wishes and caviar dreams`, 
+        `look for a great investment opportunity`, `be able to afford that avocado toast you've always wanted to try`];
+    return financialOutlooks[Math.floor(Math.random() * 4)]
+}
+
 
 const getMyHoroscope = () => {
     console.log(`Your day will be ${dayOutlook()}.`)
