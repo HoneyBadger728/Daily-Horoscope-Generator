bd8d41ac84ce551815b180f92c8990f8f4ed459c
bDavis <BlakelyJDavis@gmail.com>
2024-11-09 19:50:40 -0500

    made array randomization more dynamic

--- dailyHoroscopeGenerator.js
+++ dailyHoroscopeGenerator.js
@@ -1,19 +1,19 @@
 const dayOutlook = () => {
     const dayOutlooks = [`like a punch to the face`, `a breath of fresh air`, `full of learning opportunities`];
-    return dayOutlooks[Math.floor(Math.random() * 3)]
+    return dayOutlooks[Math.floor(Math.random() * dayOutlooks.length)]
 }
 
 const financialOutlook = () => {
     const financialOutlooks = ['expect to experience a not-so-great depression', `soon be enjoying champagne wishes and caviar dreams`, 
         `look for a great investment opportunity`, `be able to afford that avocado toast you've always wanted to try`];
-    return financialOutlooks[Math.floor(Math.random() * 4)]
+    return financialOutlooks[Math.floor(Math.random() * financialOutlooks.length)]
 }
 
 const loveOutlook = () => {
     const loveOutlooks = [`you're better off joining a convent`, 'you should remember what Whitney Houston said about the greatest love of all', 
         `think about adopting an animal from a local shelter`, `you are about to find a unicorn`, 
         `keep in mind that 'you miss 100% of the shots you don't take' - Wayne Gretzky`]
-    return loveOutlooks[Math.floor(Math.random() * 5)]
+    return loveOutlooks[Math.floor(Math.random() * loveOutlooks.length)]
 }
 
 
